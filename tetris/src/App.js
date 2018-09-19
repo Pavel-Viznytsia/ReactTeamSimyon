8d9215cc9f47591ef8adf75f802e76a871cc6cca
Pavel Viznitsa <viznitsap@gmail.com>
2018-09-19 21:43:21 +0300

    tetris completed

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -2,64 +2,38 @@ import React, { Component } from 'react';
 import ViewPort from './components/ViewPort';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor( props ) {
+    super( props );
     this.timer = 0;
     this.color = 'blue';
     this.state = {
       x: 140,
       y: 0,
       boxes: [],
-      isGameOver: false
+      isGameOver: false,
     };
   }
 
   componentDidMount() {
-    this.timer = setInterval(() => {
-      this.moveBox(0, 20); // move the box down
-    }, 100);
+    this.timer = setInterval( () => {
+      this.moveBox( 0, 20 ); // move the box down
+    }, 100 );
   }
   
   shouldComponentUpdate( nextProps, nextState ) {
-    const { x, y, boxes, isGameOver } = this.state;
-
-    boxes.forEach( box => {
-      if ( boxes.length !== 0 ) {
-        if ( box.y <= 120 ) {
-          this.setState( prevState => ( {
-            x: 140,
-            y: 0,
-            isGameOver: !prevState
-          } ) );
-          clearInterval( this.timer );
-        }
-
-        if ( nextState.x === box.x && nextState.y === box.y || nextState.y === 300 ) {
-          this.setState( {
-            x: 140,
-            y: 0,
-            boxes: boxes.concat( {
-              x: x,
-              y: y
-            } )
-          } );
-        }
-      }
-    } );
-    return !isGameOver; // by default
+    const { isGameOver } = this.state;
+    return !isGameOver;
   }
-  
-  componentDidUpdate(prevProps, prevState) {}
-  
+
   componentWillUnmount() {
-    clearInterval(this.timer);
+    clearInterval( this.timer );
   }
 
   onKeyUp = event => {
-    if (event.key === 'ArrowRight' && this.state.x < 280) {
-      this.moveBox(20, 0);
-    } else if (event.key === 'ArrowLeft' && this.state.x > 0) {
-      this.moveBox(-20, 0);
+    if ( event.key === 'ArrowRight' && this.state.x < 280 ) {
+      this.moveBox( 20, 0 );
+    } else if ( event.key === 'ArrowLeft' && this.state.x > 0 ) {
+      this.moveBox( -20, 0 );
     }
   };
 
@@ -72,16 +46,42 @@ export default class App extends Component {
         y: 0,
         boxes: boxes.concat( {
           x: x,
-          y: y
-        } )
+          y: y,
+        } ),
       } );
     } else if ( !isGameOver ) {
       this.setState( prevState => ( {
         x: prevState.x + _x,
-        y: prevState.y + _y
+        y: prevState.y + _y,
       } ) );
+      this.onPlaceExist();
     }
   };
+
+  onPlaceExist = () => {
+    const { x, y, boxes } = this.state;
+
+    if ( boxes.length !== 0 ) {
+      boxes.forEach( box => {
+        if ( box.y === 0 && y > 0 ) {
+          this.setState( {
+            x: 140,
+            y: 0,
+            isGameOver: true,
+          } );
+        } else if ( box.x === x && box.y === y || y === 300 ) {
+          this.setState( {
+            x: 140,
+            y: 0,
+            boxes: boxes.concat( {
+              x: x,
+              y: y - 20,
+            } ),
+          } );
+        }
+      } );
+    }
+  }
 
   render() {
     return (
--- tetris/src/components/ViewPort/index.jsx
+++ tetris/src/components/ViewPort/index.jsx
@@ -8,8 +8,8 @@ export default function ViewPort(props) {
   const { onKeyUp, color } = props;
   const { x, y, boxes, isGameOver} = props.state;
   return (
-    <div className="viewPort" onKeyUp={onKeyUp}>
-      {!isGameOver ? <InnerSq x={x} y={y} color={color} /> : <p>Game Over</p>}
+    <div contentEditable className="viewPort" onKeyUp={onKeyUp}>
+      {!isGameOver ? <InnerSq x={x} y={y} color={color} /> : <p className = "gameOver">Game Over</p>}
       {boxes.map((box, idx) => <InnerSq key={idx.toString()} x={box.x} y={box.y} />)}
     </div>
   );
